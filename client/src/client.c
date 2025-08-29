8a7590e416fa2c0f0eb51141ca136ab8007aeeef
santigutman <santigutman04@gmail.com>
2025-08-29 15:06:48 +0000

    Funciona hasta la etapa 2

--- client/src/client.c
+++ client/src/client.c
@@ -1,4 +1,10 @@
 #include "client.h"
+#include <commons/config.h>
+#include <commons/log.h>
+
+t_log* iniciar_logger(void);
+t_config* iniciar_config(char* path);
+
 
 int main(void)
 {
@@ -19,21 +25,27 @@ int main(void)
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
 
-
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
-	config = iniciar_config();
+	config = iniciar_config("client/cliente.config");
 
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
+	valor = config_get_string_value(config, "CLAVE");
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+
 
 	// Loggeamos el valor de config
+	log_info(logger, "Hola! Soy un log\nCLAVE: %s\nIP: %s\nPUERTO: %s", valor, ip, puerto);
 
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
-	leer_consola(logger);
-
+	//leer_consola(logger);
+	config_destroy(config);
+	log_destroy(logger);
+	return 0;
 	/*---------------------------------------------------PARTE 3-------------------------------------------------------------*/
 
 	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
@@ -54,42 +66,40 @@ int main(void)
 
 t_log* iniciar_logger(void)
 {
-	t_log* nuevo_logger;
+	t_log* nuevo_logger = log_create("tp0.log", "TP0", 1, LOG_LEVEL_INFO);
 
 	return nuevo_logger;
 }
 
-t_config* iniciar_config(void)
+t_config* iniciar_config(char* path)
 {
-	t_config* nuevo_config;
+	t_config* nuevo_config = config_create(path);
 
 	return nuevo_config;
 }
 
-void leer_consola(t_log* logger)
+t_paquete* leer_consola(t_log* logger)
 {
+	t_paquete* paquete = crear_paquete()
 	char* leido;
 
-	// La primera te la dejo de yapa
-	leido = readline("> ");
-
-	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
-
-
-	// ¡No te olvides de liberar las lineas antes de regresar!
-
+	while ((leido = readline("> ")) != NULL){
+		if(*leido == '\0'){
+			free(leido);
+			break;
+		}
+		log_info(logger, "Lei de consola: %s", leido);
+		agregar_a_paquete(paquete, leido, 10)
+		free(leido);
+	}	
 }
 
 void paquete(int conexion)
 {
-	// Ahora toca lo divertido!
-	char* leido;
-	t_paquete* paquete;
-
 	// Leemos y esta vez agregamos las lineas al paquete
-
-
-	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
+	t_paquete* paqueteAEnviar = leer_consola()
+	enviar_paquete(paqueteAEnviar, conexion)
+	eliminar_paquete(paquete)
 	
 }
 
